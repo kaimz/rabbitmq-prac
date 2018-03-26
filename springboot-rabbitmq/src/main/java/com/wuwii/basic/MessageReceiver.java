6a187df9d72563d277baa660513cfffe726e6722
kaimz <k@wuwii.com>
2018-03-26 12:00:51 +0800

    fix

--- springboot-rabbitmq/src/main/java/com/wuwii/basic/MessageReceiver.java
+++ springboot-rabbitmq/src/main/java/com/wuwii/basic/MessageReceiver.java
@@ -1,7 +1,8 @@
 package com.wuwii.basic;
 
 import com.rabbitmq.client.Channel;
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 
@@ -12,14 +13,14 @@ import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
  * @version 1.0
  * @since <pre>2018/3/19 10:37</pre>
  */
-@Log
+@Slf4j
 public class MessageReceiver implements ChannelAwareMessageListener {
 
     @Override
     public void onMessage(Message message, Channel channel) throws Exception {
         try {
-            byte[] body = message.getBody();
-            log.info(">>>>>>> receive： " + new String(body));
+            val body = message.getBody();
+            log.info(">>>>>>> receive： {}", new String(body));
         } finally {
             // 确认成功消费，否则消息会转发给其他的消费者，或者进行重试
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
